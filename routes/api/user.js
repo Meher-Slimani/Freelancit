59781d584aad09e5e6a37bff9ea26eda5bb0deed
Meher Slimani <meher.slimeni@gmail.com>
2021-02-03 16:14:43 +0100

    just to avoid an error

--- routes/api/user.js
+++ routes/api/user.js
@@ -44,7 +44,6 @@ router.post(
       let user = await User.findOne({ email });
 
       if (user) {
-        console.log(user);
         return res
           .status(400)
           .json({ errors: [{ msg: "User already exists" }] });
